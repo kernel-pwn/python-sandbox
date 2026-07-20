ccbc8b29820e4767082f1db64834acaf3892d0fc
Adeeb Mushfique <124122053+kernel-pwn@users.noreply.github.com>
2026-07-20 20:17:31 +0600

    Update madlibs.py

--- mini_projects/madlibs.py
+++ mini_projects/madlibs.py
@@ -3,7 +3,7 @@
 adjective1 = input("Enter your adjective (description): ")
 noun1 = input("Enter your noun (noun, place, thing): ")
 adjective2 = input("Enter your adjective (description): ")
-verb1 = input("Enter your verb ending with 'ing': ")
+verb1 = input("Enter your verb ending with '-ing': ")
 adjective3 = input("Enter your adjective (description): ")
 
 print(f"today i went to a {adjective1} zoo.")
